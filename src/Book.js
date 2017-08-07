89a3da8bd5d6ba0b6c2613804386687c9825721d
Ben Wright <benwright120@yahoo.com>
2017-08-06 22:33:49 -0400

    Fix: adjusted component to still work if some of the data properties are not available from the API.
    The thumbnail of the cover is not available on all books and so a few were causing errors that broke the app.

--- src/Book.js
+++ src/Book.js
@@ -5,7 +5,7 @@ class Book extends Component {
     static propTypes = {
         bookInfo: PropTypes.object.isRequired,
         onChangeShelf: PropTypes.func.isRequired
-    }
+    };
 
     render() {
         const { bookInfo } = this.props;
@@ -13,7 +13,7 @@ class Book extends Component {
         return (
           <div className="book">
             <div className="book-top">
-              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${bookInfo.imageLinks.smallThumbnail})` }}></div>
+              <div className="book-cover" style={ {width: 128, height: 193, backgroundImage: "url(" + ((bookInfo.imageLinks) ? bookInfo.imageLinks.smallThumbnail : '' )+ ")" }}></div>
               <div className="book-shelf-changer">
                 <select value={bookInfo.shelf} onChange={(event) => this.props.onChangeShelf(bookInfo, event.target.value)}>
                   <option value="none" disabled>Move to...</option>
@@ -24,8 +24,8 @@ class Book extends Component {
                 </select>
               </div>
             </div>
-            <div className="book-title">{ bookInfo.title }</div>
-            <div className="book-authors">{ bookInfo.author }</div>
+            <div className="book-title">{ bookInfo.title || "" }</div>
+            <div className="book-authors">{ bookInfo.author || "" }</div>
           </div>
         );
     }
